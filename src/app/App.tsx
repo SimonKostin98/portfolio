23ca0599d4dac1797ae80f88741857f5b98ed18d
Simon Kostin <s-kostin@t-online.de>
2020-10-14 23:14:10 +0200

    make initial project configurations

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import './App.scss';
+
+import React, { ReactElement } from 'react';
+
+
+export const App = (): ReactElement => {
+  return <div className="app">Hier kommt mein Portfolio hin</div>;
+};
