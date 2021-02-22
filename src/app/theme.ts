442a67704cc52ecf3fefc4baa99a63b9c8b444ed
Simon Kostin <s-kostin@t-online.de>
2021-02-22 20:02:07 +0100

    change theme color

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,22 +1,44 @@
 import { ThemeOptions } from '@material-ui/core';
-import { blue, indigo, orange, purple } from '@material-ui/core/colors';
+import { orange, purple } from '@material-ui/core/colors';
 
 export const lightTheme: ThemeOptions = {
   palette: {
-    primary: blue,
-    secondary: indigo,
+    primary: {
+      light: '#01FFFF',
+      main: '#CC137A',
+      dark: '#B3116B'
+    },
+    secondary: {
+      light: '#FF61BE',
+      main: '#EA55B1',
+      dark: '#EF0888'
+    },
     background: {
       paper: '#fff',
       default: '#fff',
     },
     type: 'light',
   },
+  typography:{
+    fontFamily: [
+      'Varela Round',
+      'Nunito'
+    ].join(','),
+  }
 };
 
 export const darkTheme: ThemeOptions = {
   palette: {
-    primary: orange,
-    secondary: purple,
+    primary: {
+      light: '#01FFFF',
+      main: '#00389F',
+      dark: '#000D80'
+    },
+    secondary: {
+      light: '#FF61BE',
+      main: '#EA55B1',
+      dark: '#EF0888'
+    },
     background: {
       paper: '#000',
       default: '#222',
