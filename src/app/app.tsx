dd2f390623f5b423093c57a3d15202b87b3bf5fa
Simon Kostin <s-kostin@t-online.de>
2021-02-22 20:03:27 +0100

    add new background image

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,6 @@
 import './app.scss';
 
+import backgroundURL from '@assets/images/background.png';
 import {
   createMuiTheme,
   CssBaseline,
@@ -22,14 +23,54 @@ const useDarkMode = (): [ThemeOptions, () => void] => {
   return [theme, toggleTheme];
 };
 
+const useStyles = makeStyles(() => ({
+  background: {
+    position: 'relative',
+    height: '100vh',
+    width: '100vw',
+    backgroundImage: `url(${backgroundURL as string})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  },
+  blur: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    filter: 'blur(10px)',
+    backgroundColor: 'rgba(0, 0, 0, 0.35)',
+    height: '100%',
+    width: '100%',
+    zIndex: 0,
+  },
+  mainContent: {
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    color: 'white',
+    zIndex: 1,
+  },
+}));
+
 export const App = (): ReactElement => {
   const [theme, toggleDarkMode] = useDarkMode();
   return (
     <MuiThemeProvider theme={createMuiTheme(theme)}>
       <CssBaseline />
       <ErrorBoundaryComponent>
-        <NavigationBar toggleDarkMode={toggleDarkMode} />
-        <button onClick={toggleDarkMode}>dark mode</button>
+        <div className={classes.background}>
+          <div className={classes.blur} />
+          <div className={classes.mainContent}>
+            <NavigationBar
+              toggleDarkMode={toggleDarkMode}
+              active={active}
+              setActive={(newValue: number) => setActive(newValue)}
+            />
+            <Footer />
+          </div>
+        </div>
       </ErrorBoundaryComponent>
     </MuiThemeProvider>
   );
