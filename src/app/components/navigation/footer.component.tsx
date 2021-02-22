20269640bcbd2c46c4bd969aa7428096001eb2c6
Simon Kostin <s-kostin@t-online.de>
2021-02-22 22:19:13 +0100

    add social media icons and settings button to footer

--- src/app/components/navigation/footer.component.tsx
+++ src/app/components/navigation/footer.component.tsx
@@ -1,4 +1,9 @@
-import { makeStyles } from '@material-ui/core';
+import { IconButton, makeStyles } from '@material-ui/core';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailIcon from '@material-ui/icons/Mail';
+import SettingsIcon from '@material-ui/icons/Settings';
 import React, { ReactElement } from 'react';
 
 interface IFooterProps {}
@@ -9,12 +14,38 @@ const useStyles = makeStyles(() => ({
     left: 0,
     bottom: 0,
     width: '100%',
-    height: '4vh',
     borderTop: '1px solid rgba(255, 255, 255, .7)',
+    color: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  settingsIcon: {
+    position: 'absolute',
+    right: 0,
   },
 }));
 
 export const Footer = (props: IFooterProps): ReactElement => {
   const classes = useStyles();
-  return <footer className={classes.footer}>Hier steht Text</footer>;
+  return (
+    <footer className={classes.footer}>
+      <IconButton color="inherit">
+        <GitHubIcon />
+      </IconButton>
+      <IconButton color="inherit">
+        <FacebookIcon />
+      </IconButton>
+      <IconButton color="inherit">
+        <LinkedInIcon />
+      </IconButton>
+      <IconButton color="inherit">
+        <MailIcon />
+      </IconButton>
+      <IconButton color="inherit" className={classes.settingsIcon}>
+        <SettingsIcon />
+      </IconButton>
+    </footer>
+  );
 };
