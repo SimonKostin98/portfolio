8a7064c61763b395f2f3fe730006c7764811005d
Simon Kostin <s-kostin@t-online.de>
2021-04-15 01:50:15 +0200

    add navigation bar for resume component

--- src/app/components/navigation/navigationBar.component.tsx
+++ src/app/components/navigation/navigationBar.component.tsx
@@ -1,9 +1,14 @@
 import logoLight from '@assets/images/logoLight.png';
 import { AppBar, makeStyles, Tab, Tabs, Theme } from '@material-ui/core';
+import AddBoxIcon from '@material-ui/icons/AddBox';
 import AssignmentIcon from '@material-ui/icons/Assignment';
+import BuildIcon from '@material-ui/icons/Build';
+import CodeIcon from '@material-ui/icons/Code';
 import HomeIcon from '@material-ui/icons/Home';
 import ImportantDevicesIcon from '@material-ui/icons/ImportantDevices';
 import InfoIcon from '@material-ui/icons/Info';
+import SchoolIcon from '@material-ui/icons/School';
+import WorkIcon from '@material-ui/icons/Work';
 import clsx from 'clsx';
 import React, { ReactElement } from 'react';
 
@@ -47,6 +52,19 @@ const useStylesSmall = makeStyles((theme: Theme) => ({
     boxShadow: 'none',
   },
   menuItem: { color: 'white' },
+}));
+
+const useStylesResume = makeStyles((theme: Theme) => ({
+  appBar: {
+    boxShadow: 'none',
+    borderBottom: `1px solid ${theme.palette.background.default}`,
+  },
+
+  menuItem: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 'medium',
+  },
 }));
 
 export const NavigationBar = (props: INavigationBarProps): ReactElement => {
@@ -122,6 +140,38 @@ export const NavigationBarSmall = (
           label="Projects"
           className={classes.menuItem}
         />
+      </Tabs>
+    </AppBar>
+  );
+};
+
+export const NavigationBarResume = (
+  props: INavigationBarProps,
+): ReactElement => {
+  const classes = useStylesResume();
+
+  const handleChange = (
+    _event: React.ChangeEvent<unknown>,
+    newValue: number,
+  ) => {
+    props.setActive(newValue);
+  };
+  return (
+    <AppBar position="sticky" color="transparent" className={classes.appBar}>
+      <Tabs
+        indicatorColor="primary"
+        textColor="primary"
+        value={props.active}
+        onChange={handleChange}
+        selectionFollowsFocus
+        variant="fullWidth"
+        centered
+      >
+        <Tab icon={<WorkIcon />} className={classes.menuItem} />
+        <Tab icon={<SchoolIcon />} className={classes.menuItem} />
+        <Tab icon={<CodeIcon />} className={classes.menuItem} />
+        <Tab icon={<BuildIcon />} className={classes.menuItem} />
+        <Tab icon={<AddBoxIcon />} className={classes.menuItem} />
       </Tabs>
     </AppBar>
   );
