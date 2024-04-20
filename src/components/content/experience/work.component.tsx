bb97262a3d1314605948eee1ec65d5bb7f714cc5
Simon Kostin <s-kostin@t-online.de>
2024-04-20 03:09:20 +0200

    redesign experience view #39

--- src/components/content/experience/work.component.tsx
+++ src/components/content/experience/work.component.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable max-len */
+
 import bmwLogo from '@assets/work/BMW.webp';
 import broseLogo from '@assets/work/Brose.webp';
 import ecomioLogo from '@assets/work/Ecomio.webp';
@@ -7,112 +8,115 @@ import { Theme } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import React, { ReactElement, useState } from 'react';
 
-import { ExperienceNavigation } from './experienceNavigation.component';
-import { WorkContent } from './workContent.component';
+import { WorkAccordion } from './workAccordion.component';
 
 const useStyles = makeStyles((theme: Theme) => ({
   workComponent: {
     width: '100%',
     height: '100%',
     display: 'flex',
-
-    [theme.breakpoints.down('md')]: {
-      flexDirection: 'column',
+    flexDirection: 'column',
+    gap: 50,
+    [theme.breakpoints.down('lg')]: {
+      alignItems: 'center',
     },
   },
-  workContent: {
-    width: 'calc(100% - 200px)',
-    height: '100%',
-    position: 'relative',
-    left: 50,
-    border: `2px solid ${theme.palette.primary.main}`,
-    borderRadius: 20,
+  workComponentItem: {
+    width: '85%',
 
-    [theme.breakpoints.down('md')]: {
-      width: '100%',
-      height: 'calc(100% - 75px)',
-      left: 0,
-      top: 15,
+    '&:nth-child(odd)': {
+      [theme.breakpoints.up('lg')]: {
+        marginLeft: '15%',
+      },
     },
   },
 }));
 
 export const WorkComponent = (): ReactElement => {
   const classes = useStyles();
-  const [activeElement, setActiveElement] = useState(0);
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleAccordionChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      console.log('CALLED');
+      setExpanded(isExpanded ? panel : false);
+    };
   return (
     <div className={classes.workComponent}>
-      <ExperienceNavigation
-        elements={[
-          { title: 'eco.mio', iconUrl: ecomioLogo as string },
-          { title: 'BMW', iconUrl: bmwLogo as string },
-          { title: 'Brose', iconUrl: broseLogo as string },
-          { title: 'Haba', iconUrl: habaLogo as string },
-        ]}
-        activeElement={activeElement}
-        setActiveElement={setActiveElement}
-      />
-      <div className={classes.workContent}>
-        {activeElement == 0 && (
-          <WorkContent
-            role="Full Stack Developer"
-            company="eco.mio"
-            companyLink="https://ecomio.com/"
-            type="Working Student"
-            duration={['Sep 2021 - May 2022']}
-            description={[
-              'Implemented a broad range of web application features such as frontend views, backend APIs, database models as well as authenication and security aspects with third party services',
-              'Developed a complete Infrastructure as Code setup for all backend services using terraform',
-              'Set up a Continous Integration workflow',
-              'Regularly reviewed both frontend and backend code of other developers',
-            ]}
-          />
-        )}
-        {activeElement == 1 && (
-          <WorkContent
-            role="Full Stack Developer"
-            company="BMW"
-            companyLink="https://www.bmwgroup.com/"
-            type="Internship"
-            duration={['Aug 2020 - Oct 2020']}
-            description={[
-              'Carried out the analysis, design, implementation and launch of a generic anomaly detection pipeline for vehicle streaming data',
-              'Developed a full stack web application using Kotlin, React and the AWS SDK',
-              'Worked in an agile team of 5 working students and collaborated with engineers and developers from various departments ',
-            ]}
-          />
-        )}
-        {activeElement == 2 && (
-          <WorkContent
-            role="Front End Developer"
-            company="Brose"
-            companyLink="https://www.brose.com/"
-            type="Vacation Work"
-            duration={[
-              'Aug 2016 - Oct 2016 | Mar 2017 - Apr 2017 ',
-              'Aug 2017 - Sep 2017 | Mar 2018 - Apr 2018',
-              'Sep 2018 - Oct 2018 | Mar 2019 - Apr 2019',
-            ]}
-            description={[
-              'Developed a web interface for an inhouse vehicle database',
-              'Worked with established technologies such as HTML, CSS and Javascript',
-              'Cooperated with specialists from various fields of vehicle development',
-            ]}
-          />
-        )}
-        {activeElement == 3 && (
-          <WorkContent
-            role="IT Intern"
-            company="Haba"
-            companyLink="https://www.haba-play.com/"
-            type="Internship"
-            duration={['Oct 2013']}
-            description={[
-              'Learned basic programming principles and algorithms with Java',
-              'Gained first insights into the everyday professional life of a software developer',
-            ]}
-          />
-        )}
+      <div className={classes.workComponentItem}>
+        <WorkAccordion
+          role="Full Stack Developer"
+          company="eco.mio"
+          companyLink="https://ecomio.com/"
+          type="Working Student (Sep 2021 - May 2022) | Full Time (Nov 2022 - Today)"
+          duration={['Sep 2021 - May 2022 | Nov 2022 - Today']}
+          description={[
+            'Implemented a broad range of web application features such as frontend views, backend APIs, database models as well as authenication and security aspects with third party services',
+            'Developed a complete Infrastructure as Code setup for all backend services using terraform',
+            'Set up a Continous Integration workflow',
+            'Regularly reviewed both frontend and backend code of other developers',
+          ]}
+          image={ecomioLogo as string}
+          index={0}
+          handleAccordionChange={handleAccordionChange('workAccordion0')}
+          expanded={expanded === 'workAccordion0'}
+        />
+      </div>
+      <div className={classes.workComponentItem}>
+        <WorkAccordion
+          role="Full Stack Developer"
+          company="BMW"
+          companyLink="https://www.bmwgroup.com/"
+          type="Internship"
+          duration={['Aug 2020 - Oct 2020']}
+          description={[
+            'Carried out the analysis, design, implementation and launch of a generic anomaly detection pipeline for vehicle streaming data',
+            'Developed a full stack web application using Kotlin, React and the AWS SDK',
+            'Worked in an agile team of 5 working students and collaborated with engineers and developers from various departments ',
+          ]}
+          image={bmwLogo as string}
+          index={1}
+          handleAccordionChange={handleAccordionChange('workAccordion1')}
+          expanded={expanded === 'workAccordion1'}
+        />
+      </div>
+      <div className={classes.workComponentItem}>
+        <WorkAccordion
+          role="Front End Developer"
+          company="Brose"
+          companyLink="https://www.brose.com/"
+          type="Vacation Work"
+          duration={[
+            'Aug 2016 - Oct 2016 | Mar 2017 - Apr 2017 | Aug 2017 - Sep 2017',
+            'Mar 2018 - Apr 2018 | Sep 2018 - Oct 2018 | Mar 2019 - Apr 2019',
+          ]}
+          description={[
+            'Developed a web interface for an inhouse vehicle database',
+            'Worked with established technologies such as HTML, CSS and Javascript',
+            'Cooperated with specialists from various fields of vehicle development',
+          ]}
+          image={broseLogo as string}
+          index={2}
+          handleAccordionChange={handleAccordionChange('workAccordion2')}
+          expanded={expanded === 'workAccordion2'}
+        />
+      </div>
+      <div className={classes.workComponentItem}>
+        <WorkAccordion
+          role="IT Intern"
+          company="Haba"
+          companyLink="https://www.haba-play.com/"
+          type="Internship"
+          duration={['Oct 2013']}
+          description={[
+            'Learned basic programming principles and algorithms with Java',
+            'Gained first insights into the everyday professional life of a software developer',
+          ]}
+          image={habaLogo as string}
+          index={3}
+          handleAccordionChange={handleAccordionChange('workAccordion3')}
+          expanded={expanded === 'workAccordion3'}
+        />
       </div>
     </div>
   );
